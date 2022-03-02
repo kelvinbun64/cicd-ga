86755de855b18a13cbc16c33359e25070d802f15
kelvin-bun <kelvin.bun@okhome.id>
2022-03-02 14:13:11 +0700

    fix server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const {greeting} = require('./user')
 
 app.get('/', (req,res) => res.send('Hello This is Project CI/CD'))
 app.get('/sonar', (req,res) => res.send('Hello, Sonar !!!'))
